e089a6f6a04b5817af965844fd807d86b186fe82
Pablo Lencinas <pslencinas@gmail.com>
2019-05-30 20:15:41 -0300

    Subiendo mas ejercicios- May19

--- 03-Estructuras/Estruct_carga de una estructura.c
+++ 03-Estructuras/Estruct_carga de una estructura.c
@@ -1,43 +1,37 @@
 #include<stdio.h>
-#define cantidad 3
 
 struct info_archivo
 {
-char nombre[20];
-int extension;
-char permisos[15];
+    char nombre[20];
+    char apellido[20];
+    int edad;
+    char sexo;
 
 };
-struct info_archivo ARCHIVOS[cantidad];
+
 int main(void)
 {
 
-int i;
-/* Lazo para la entrada de datos */
-for(i=0;i<cantidad;i++)
-{
+    struct info_archivo X;
+    int i;
+
     printf("\nIngrese nombre: ");
     fflush(stdin);
-    gets(ARCHIVOS[i].nombre);
-    printf("Ingrese extension: ");
+    gets(X.nombre);
+    printf("\nIngrese apellido: ");
     fflush(stdin);
-    scanf("%d", &ARCHIVOS[i].extension);
-    printf("Ingrese permisos de discado: ");
+    gets(X.apellido);
+    printf("Ingrese edad: ");
+    scanf("%d", &X.edad);
+    printf("Ingrese sexo (m/f): ");
     fflush(stdin);
-    gets(ARCHIVOS[i].permisos);
-
-}
-/* Lazo para la presentación de datos */
-
-for(i=0;i<cantidad;i++)
-{
-    printf("\nNombre: %5s\t Extension: %5d\t Permisos: %5s",
-    ARCHIVOS[i].nombre, ARCHIVOS[i].extension, ARCHIVOS[i].permisos);
-
-}
+    X.sexo = getchar()
 
 
+    printf("\nNombre: %s\t Apellido: %s\t Edad: %d\t Sexo: %c",
+        X.nombre, X.apellido, X.edad, X.sexo);
 
+    
 printf("\n\n");
 return 0;
 }
